d652c287fc3ab7cfcb6e8ec17c62d0a17569a41d
luzhangtina <zhanglu.xidian@gmail.com>
2025-02-17 23:35:20 +1100

    update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://luzhangtina.github.io/lucidiary/',
+    site: import.meta.env.MODE === 'production' ? 'https://luzhangtina.github.io/lucidiary' : 'http://localhost:4321',
+    base: import.meta.env.MODE === 'production' ? '/lucidiary/' : '/',
 });
+
+console.log(import.meta.env.BASE_URL);
